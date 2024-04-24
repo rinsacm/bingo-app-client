5da6e59bdd9a7d06800b38012236ce398820429e
Rinsa Fathima C M <rinsafathima09@gmail.com>
2024-04-24 14:16:26 +0530

    gap in ui

--- src/components/BingoSquares.js
+++ src/components/BingoSquares.js
@@ -286,7 +286,7 @@ const BingoSquares = () => {
           </div>
         </div>
       )}
-      <div className="text-center my-4 text-red-500 font-thin font-kanit text-xl">
+      <div className="text-center my-2 text-red-500 font-thin font-kanit text-xl">
         {isMyTurn ? "Your Turn" : ""}
       </div>
 
@@ -338,9 +338,12 @@ const BingoSquares = () => {
         </div>
       )}
 
-      <div className="text-center text-black-600 flex justify-center w-screen text-lg my-2">
+      <div
+        className="text-center text-black-600 flex justify-center w-screen text-lg my-2"
+        mx-2
+      >
         <div>
-          <div className="grid grid-cols-2 grid-rows-2 gap-2">
+          <div className="grid grid-cols-3 grid-rows-2 gap-2">
             <label>Name: </label>
             {!isStartEnabled ? (
               <input
@@ -351,7 +354,7 @@ const BingoSquares = () => {
                   e.preventDefault();
                   setName(e.target.value);
                 }}
-                className="g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className=" col-span-2 g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               />
             ) : (
               <div>{name}</div>
@@ -366,7 +369,7 @@ const BingoSquares = () => {
                   e.preventDefault();
                   setRoom(e.target.value);
                 }}
-                className="g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className=" col-span-2 g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               />
             ) : (
               <div>{room}</div>
